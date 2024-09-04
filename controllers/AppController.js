5802726fc57cf87f993fa5b11bc19be1779cb7d7
OG-1534 <gershomochanda@gmail.com>
2024-09-04 23:54:20 +0300

    First API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,6 +1,7 @@
 import redisClient from '../utils/redis.js';
 import dbClient from '../utils/db.js';
 
+
 class AppController {
   static getStatus(req, res) {
     res.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
